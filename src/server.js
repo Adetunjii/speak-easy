9323da404f1fa713ecff924929c8caf626db3b2f
Adetunjii <adetunjithomas1@gmail.com>
2021-01-08 14:28:44 +0100

    removed optional operator

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ io.on("connect", (socket) => {
       .emit("message", { user: "admin", text: `${user.name} has joined!` });
 
     io.to(user.room).emit("roomData", {
-      room: user?.room,
+      room: user.room,
       users: getUsersInRoom(user?.room),
     });
 
@@ -53,7 +53,7 @@ io.on("connect", (socket) => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user?.room).emit("message", { user: user?.name, text: message });
+    io.to(user?.room).emit("message", { user: user.name, text: message });
 
     callback();
   });
@@ -68,7 +68,7 @@ io.on("connect", (socket) => {
       });
       io.to(user?.room).emit("roomData", {
         room: user?.room,
-        users: getUsersInRoom(user?.room),
+        users: getUsersInRoom(user.room),
       });
     }
   });
